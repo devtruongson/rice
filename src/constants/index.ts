4e4e23c5af80b74b7de78ead7c7ba4c3bc3cb2f8
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-03-28 18:46:44 +0700

    update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,12 @@
+import { routesMap } from '../routes/routes';
+
 export const personalBrand = 'Cassava';
 export const HEADER_HEIGHT = 130;
 export const NAVBAR_WIDTH = 300;
 
 export const listTransitFirst = [
     { label: 'Sequence search (BLAST)', path: '/' },
-    { label: 'GlycineMine', path: '/' },
+    { label: 'GlycineMine', path: 'https://dscensor.legumeinfo.org/multiqc-demo/genome_main:Glycine' },
     { label: 'Genetic & trait maps', path: '/' },
     { label: 'GWAS viewer', path: '/' },
     { label: 'Germplasm GISs', path: '/' },
@@ -22,7 +24,7 @@ export const listTransitSecond = [
 
 export const listTranstLast = [
     { label: 'Pangenes / gene translator', path: '/' },
-    { label: 'GO enrichment', path: '/' },
+    { label: 'GO enrichment', path: routesMap.Go },
     { label: 'Genotype comparisons', path: '/' },
     { label: 'cross-species QTL/GWASs', path: '/' },
     { label: 'Parentage & pedigrees', path: '/' },
