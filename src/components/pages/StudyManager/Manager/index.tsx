fa3954a8930bf83b6bd58ebbc50d5322dd61229a
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-04-20 14:41:31 +0700

    add modal detail info study

--- src/components/pages/StudyManager/Manager/index.tsx
+++ src/components/pages/StudyManager/Manager/index.tsx
@@ -1,4 +1,18 @@
-import { Box, Divider, HStack, Text, useDisclosure } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    Divider,
+    HStack,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Text,
+    useDisclosure,
+} from '@chakra-ui/react';
 import ConfirmDelete from '../../../molecules/ConfirmDelete';
 import TableCusTom from '../../../molecules/Table';
 import ActionCustom from '../../../molecules/ActionCustom';
@@ -19,6 +33,8 @@ const Manager = () => {
     const pageSize = useMemo(() => Number(searchParams.get('pageSize')) || 10, [searchParams]);
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [idDelete, setIdDelete] = useState<string>('');
+    const { isOpen: isOpenModal, onOpen: onOpenModal, onClose: onCloseModal } = useDisclosure();
+    const [indexShow, setIndexShow] = useState(0);
 
     const deleteSpecies = useDeleteStudy({
         mutationConfig: {
@@ -44,12 +60,16 @@ const Manager = () => {
     const { data, refetch } = useGetStudies({ rest: { page: page, pageSize: pageSize } });
     const studies = useMemo(
         () =>
-            data?.data?.data?.map((item: StudyResType) => {
+            data?.data?.data?.map((item: StudyResType, index: number) => {
                 return {
                     ...item,
                     species: item?.species?.name || '',
                     action: (
                         <ActionCustom
+                            actionShow={() => {
+                                setIndexShow(index);
+                                onOpenModal();
+                            }}
                             actionDelete={() => {
                                 setIdDelete(item._id);
                                 onOpen();
@@ -73,19 +93,43 @@ const Manager = () => {
                     mt={4}
                     columns={[
                         { key: 'study_name', label: 'Study Name', w: '10%' },
-                        { key: 'link_detail_study', label: 'Link Detail', w: '10%' },
                         { key: 'study_type', label: 'Type', w: '10%' },
                         { key: 'publication_id', label: 'Publication ID', w: '10%' },
                         { key: 'author', label: 'Author', w: '10%' },
                         { key: 'synopsis', label: 'Synopsis', w: '10%' },
-                        { key: 'description', label: 'Description', w: '10%' },
                         { key: 'genotypes', label: 'Genotypes', w: '10%' },
-                        // { key: 'traits', label: 'Traits', w: '10%' },
-                        // { key: 'species', label: 'Species', w: '10%' },
                         { key: 'action', label: '', w: '5%' },
                     ]}
                     data={studies}
                 />
+
+                <Modal isOpen={isOpenModal} onClose={onCloseModal} size="4xl">
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Detailed information about study</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Box>
+                                <InfoCommon title="study_name" content={studies[indexShow]?.study_name} />
+                                <InfoCommon title="link_detail_study" content={studies[indexShow]?.link_detail_study} />
+                                <InfoCommon title="study_type" content={studies[indexShow]?.study_type} />
+                                <InfoCommon title="publication_id" content={studies[indexShow]?.publication_id} />
+                                <InfoCommon title="author" content={studies[indexShow]?.author} />
+                                <InfoCommon title="description" content={studies[indexShow]?.description} />
+                                <InfoCommon title="synopsis" content={studies[indexShow]?.synopsis} />
+                                <InfoCommon title="genotypes" content={studies[indexShow]?.genotypes} />
+                                <InfoCommon title="species" content={studies[indexShow]?.species} />
+                                <InfoCommon title="traits" content={studies[indexShow]?.traits} />
+                            </Box>
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button colorScheme="blue" mr={3} onClick={onCloseModal}>
+                                Close
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
 
                 <ConfirmDelete
                     header="Xóa Species"
@@ -101,3 +145,20 @@ const Manager = () => {
 };
 
 export default Manager;
+
+type InfoCommonType = {
+    title: string;
+    content: string;
+};
+const InfoCommon = ({ title, content }: InfoCommonType) => {
+    return (
+        <Box mb={4}>
+            <Text fontWeight={500} textTransform="capitalize">
+                {title}
+            </Text>
+            <Text textIndent="4px" fontWeight={300}>
+                {content}
+            </Text>
+        </Box>
+    );
+};
